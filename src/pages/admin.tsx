4559b7788cb15f451be9766991ce180f61da7ba1
Zippys <id1366351@outloo.com>
2025-05-05 12:13:40 -0300

    05/05/25

---
+++ src/pages/admin.tsx
@@ -0,0 +1,13 @@
+// pages/admin.tsx
+
+import React from 'react';
+
+const AdminPage = () => {
+  return (
+    <div>
+      <h1>Página de Administração</h1>
+    </div>
+  );
+};
+
+export default AdminPage;
