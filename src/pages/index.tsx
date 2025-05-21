89d14ca1d8268b37c1f2f416968678cd248a3fbf
Zippys <id1366351@outloo.com>
2025-05-21 09:19:14 -0300

    Projetos18

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -201,15 +201,14 @@ export default function Home({ projetos }: HomeProps) {
   <h2 className="text-2xl font-bold text-center mb-6">Projetos em Destaque 🚀</h2>
   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
     {projetos
-      //.filter(p => p.nome !== 'Sem nome' && p.imagem) // Filtra projetos válidos
+      .filter(p => p.nome !== 'Sem nome' && p.imagem) // Filtra projetos válidos
       .map(p => (
         <a
           key={p.id}
           href={p.link ?? '#'}
       target={p.link ? '_blank' : undefined}
       rel="noopener noreferrer"
-    className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-xl transition-shadow"
-    >
+    className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-xl transition-shadow">
        {p.imagem && (
             <img 
               src={p.imagem} 
